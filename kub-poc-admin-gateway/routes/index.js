de92a9ba3cf2a47d0438d82be8e61d062bc4ca65
Ced <cdegallaix@gmail.com>
2018-01-30 13:52:28 +0100

    admin gw + admin endpont

---
+++ kub-poc-admin-gateway/routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+const request = require('request');
+const fs = require('fs');
+
+let token = null;
+
+fs.readFile('/tmp/public-token/value', 'utf-8', (err, data) => {
+  if(!data){
+    console.error('No secret found');
+  }
+
+  token = data;
+})
+
+router.get('/', function(req, res, next) {
+  console.log('token', token);
+  const url = `http://kub-poc-product-service.default.svc.cluster.local:81${req.originalUrl}`
+  console.log(`Calling ${url} ...`);
+  return req.pipe(request(url)).pipe(res);  
+});
+
+router.get('/health', function(req, res, next) {
+  return res.sendStatus(200);
+});
+
+module.exports = router;

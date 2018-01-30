04871816938fd14f9689b9972810f00893e1bca3
Ced <cdegallaix@gmail.com>
2018-01-30 11:30:50 +0100

    Add secrets

--- kub-poc-gateway/routes/index.js
+++ kub-poc-gateway/routes/index.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
 const request = require('request');
+const fs = require('fs');
+
+let token = null;
+
+fs.readFile('/tmp/public-token/value', 'utf-8', (err, data) => {
+  if(!data){
+    console.error('No secret found');
+  }
+
+  token = data;
+})
 
 router.get('/', function(req, res, next) {
+  console.log('token', token);
   const url = `http://kub-poc-product-service.default.svc.cluster.local${req.originalUrl}`
   console.log(`Calling ${url} ...`);
   return req.pipe(request(url)).pipe(res);  

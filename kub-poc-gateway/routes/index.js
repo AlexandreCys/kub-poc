3e1b5f09ce2dd7fa8eed38ffcb77f3d76366b7ee
Ced <cdegallaix@gmail.com>
2018-01-30 10:09:41 +0100

    fix gateway

--- kub-poc-gateway/routes/index.js
+++ kub-poc-gateway/routes/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
 
 router.get('/', function(req, res, next) {
+  console.log('Calling http://kub-poc-product-service.default.svc.cluster.local ...');
   return req.pipe(request('http://kub-poc-product-service.default.svc.cluster.local')).pipe(res);  
+});
+
+router.get('/health', function(req, res, next) {
+  return res.sendStatus(200);
 });
 
 module.exports = router;

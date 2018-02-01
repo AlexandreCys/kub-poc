40e8d4af5f9e2e127b8e8b0ac481810e64f91d3c
Ced <cdegallaix@gmail.com>
2018-02-01 10:42:42 +0100

    Add jobloss product

--- kub-poc-jobloss/app.js
+++ kub-poc-jobloss/app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+var controllers = require('./controllers');
 
 var app = express();
 
@@ -16,9 +16,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', controllers);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -28,14 +27,16 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+  console.error(err);
+  const status = err.status || 500;
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(status)
+    .json({
+      status,
+      message: (status === 500) ? undefined : err.message,
+    });
 });
 
 module.exports = app;
---
+++ kub-poc-jobloss/controllers/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/be/contracts/:id/cancel', function (req, res, next) {
+  console.log('jobloss::', 'received call for cancelling contract (open-api)', req.params.id);
+
+  res.json({
+    'value': `successfully cancelled contract (open-api) ${req.params.id}`,
+  });
+});
+
+router.get('/admin/contracts/:id/cancel', function (req, res, next) {
+  console.log('jobloss::', 'received call for cancelling contract (admin)', req.params.id);
+
+  res.json({
+    'value': `successfully cancelled contract (admin) ${req.params.id}`,
+  });
+});
+
+router.get('/health', function(req, res, next) {
+  return res.sendStatus(200);
+});
+
+module.exports = router;
